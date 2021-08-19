9948c18482273eb409471b0f22c084a0f307ec1d
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-19 19:46:23 +0600

    date object

---
+++ date.js
@@ -0,0 +1,10 @@
+// A simple introduction to Date object in JavaScript
+
+const myFavDate = new Date('1971-12-16');
+// console.log(myFavDate);
+const anotherDate = new Date(1971, 3, 26, 11, 50, 40, 80);
+// console.log(anotherDate);
+
+if (myFavDate.getTime() < anotherDate.getTime()) {
+    console.log('favorite is earlier')
+}
