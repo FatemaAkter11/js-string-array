d257e3fa562b21eb636484828d8274a69ef858dd
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-19 11:17:04 +0600

    Search includes, indexOf, startswith, endswith

---
+++ string-search.js
@@ -0,0 +1,48 @@
+//Apply Search includes, indexOf, startswith, endswith
+const products = [
+    'Dell hardcore i29 200GB laptop',
+    'iphone 1TB camera flashlight Phone',
+    'yellow laptop with black camera',
+    'Dell 1X59 Lenovo commercial yoga laptop',
+    'LG supernova laptop Dell',
+    'HTC low price Phone',
+    'Dell purple color phone with Laptop'
+];
+
+//const searching = 'laptop';
+const searching = 'dell';
+
+// indexOf
+const output = [];
+for (const product of products) {
+    if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+        //output.push(product);
+    }
+}
+//console.log(output);
+
+//includes
+for (const product of products) {
+    if (product.toLowerCase().includes(searching.toLowerCase())) {
+        // output.push(product);
+    }
+}
+//console.log(output);
+
+//starts with
+for (const product of products) {
+    if (product.toLowerCase().startsWith(searching.toLowerCase())) {
+        // output.push(product);
+    }
+}
+
+//console.log(output);
+
+//ends with
+for (const product of products) {
+    if (product.toLowerCase().endsWith(searching.toLowerCase())) {
+        output.push(product);
+    }
+}
+
+console.log(output);
