2192081bb52f7cac5bbcbd81c6fbd2af82fbeb3a
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-19 14:26:23 +0600

    Unlimited parameters to a function using arguments

---
+++ argument.js
@@ -0,0 +1,23 @@
+//Unlimited parameters to a function using arguments
+function addNumbers(num1, num2) {
+    // console.log(arguments[1]);
+    let result = 0;
+    for (const num of arguments) {
+        result = result + num;
+    }
+    // const result = num1 + num2;
+    return result;
+}
+const sum = addNumbers(23, 13, 50, 100, 45, 76, 198, 901);
+// console.log(sum);
+
+
+function getFullName(firstName, lastName) {
+    let fullName = '';
+    for (const part of arguments) {
+        fullName = fullName + part + ' ';
+    }
+    return fullName;
+}
+const name = getFullName('Omuk', 'Songket', 'bin', 'Hanif', 'Songket', 'bin', 'Tomuk', 'Songket', 'bin', 'Kumuk', 'Songket');
+console.log(name);
