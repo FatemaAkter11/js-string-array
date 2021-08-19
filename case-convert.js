8ac408dd1a668a7a53eb2c86f19a3667645303f3
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-19 10:43:05 +0600

    toLowerCase toUpperCase

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+//String comparison using toLowerCase toUpperCase
+
+const userInput = 'bLackPinK';
+const userName = 'blackPink';
+//console.log(userInput.toLowerCase());
+//console.log(userName.toLowerCase());
+//console.log(userInput.toUpperCase());
+//console.log(userName.toUpperCase());
+if (userInput.toLowerCase() == userName.toLowerCase()) {
+    console.log('User Exits');
+}
