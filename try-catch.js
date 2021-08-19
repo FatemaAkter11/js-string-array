4a131530b49884913c6e3c1e0462bdf35e9b3a16
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-19 14:45:46 +0600

    Different types of Error object and explore Try-catch

---
+++ try-catch.js
@@ -0,0 +1,21 @@
+//Different types of Error object and explore Try-catch
+
+const myName = 'ami';
+const student = undefined;
+
+// console.log(student.name);
+
+try {
+    myName = 'tumi';
+}
+catch (error) {
+    //console.log('getting error', error);
+}
+console.log(100);
+console.log(myName);
+
+// for (let i = 0; i < 10 i++) {
+
+// }
+// name = 'tumi';
+// console.log(name);
